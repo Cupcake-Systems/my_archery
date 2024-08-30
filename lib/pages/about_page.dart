a07a0864a003711b0d646a3fcdc0de4296407ca3
Finn Drünert <finndruenert@gmail.com>
2024-08-30 10:46:52 +0200

    add publisher to about page, add font awesome icons

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:my_archery/main.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
@@ -11,39 +12,83 @@ class AboutPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('About'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const SizedBox(height: 16),
-            const Text(
-              'Entwickler',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  const Text(
+                    'Entwickler',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  ListTile(
+                    title: const Text("Finn Drünert"),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton.filledTonal(
+                          onPressed: () => launchUrlString("https://github.com/Finnomator"),
+                          icon: const Icon(FontAwesomeIcons.github),
+                        ),
+                        IconButton.filledTonal(
+                          onPressed: () => launchUrlString("https://finn.druenert.com"),
+                          icon: const Icon(FontAwesomeIcons.globe),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 8),
-            ListTile(
-              title: const Text("Finn Drünert"),
-              trailing: ElevatedButton.icon(
-                onPressed: () {
-                  launchUrlString("https://finn.druenert.com");
-                },
-                label: const Text("Website"),
-                icon: const Icon(Icons.link),
+          ),
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  const Text(
+                    'Publisher',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  ListTile(
+                    title: const Text("Cupcake Systems"),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton.filledTonal(
+                          onPressed: () => launchUrlString("https://github.com/Cupcake-Systems"),
+                          icon: const Icon(FontAwesomeIcons.github),
+                        ),
+                        IconButton.filledTonal(
+                          onPressed: () => launchUrlString("https://cupcake-systems.com"),
+                          icon: const Icon(FontAwesomeIcons.globe),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
-            const Spacer(),
-            Center(
-              child: Text(
-                'App Version ${packageInfo.version} Build ${packageInfo.buildNumber}',
-                style: const TextStyle(color: Colors.grey),
-              ),
+          ),
+          const Spacer(),
+          Center(
+            child: Text(
+              'App Version ${packageInfo.version} Build ${packageInfo.buildNumber}',
+              style: const TextStyle(color: Colors.grey),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
